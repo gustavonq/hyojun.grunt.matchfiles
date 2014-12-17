dd2b890f5fffda99d6686bc68a620f4df0575e66
igor <igor.p.almeida@gmail.com>
2014-12-17 10:38:57 -0200

    removing user's config from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,7 @@
 module.exports = function(grunt) {
 'use strict';
 	grunt.initConfig({
-		"matchfiles" : {
-			"desktop": {
-				"hosts" : ["http://static1.netshoes.net"],
-				"svn" : {
-					"url" : "http://192.168.172.127:8080/svn/Netshoes/branch/{{branch}}/netshoes/modules/estore/j2ee/Netshoes.war",
-					"inspect" : [{
-						"path" : "/assets/js-min",
-						"match" : "\.js$"
-					}]
-				}
-			}
-		}
+		matchfiles: grunt.file.readJSON("matchfiles.json")
 	});
 	grunt.loadTasks('task');
 };
