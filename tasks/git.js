f6bf14c9e6c73dbc3353205be53a9773f3bdc916
igor <igor.p.almeida@gmail.com>
2015-03-18 11:15:22 -0300

    - log improived - md5 check working fine and handling only versioned files - read to be renamed and work with svn as well

--- tasks/git.js
+++ tasks/git.js
@@ -13,6 +13,8 @@ exports.list_files = function (config, done, grunt) {
 
 	var spawn = require("child_process").spawn;
 	var path_to_list = [], file_list = [];
+	var	rev = grunt.option("rev")||"HEAD";
+	grunt.log.writeln(("Listing files on rev: " + rev).yellow);
 
 	if (config.inspect) {
 		forEach(config.inspect, function(value){
@@ -37,6 +39,7 @@ exports.list_files = function (config, done, grunt) {
 
 	function inspect_next_path () {
 		if (!!path_to_list && !path_to_list.length) {
+			grunt.log.writeln("_____________________".grey);
 			grunt.log.writeln((file_list.length + " files found matching the pattern.").grey);
 			grunt.log.writeln();
 			grunt.log.writeln("Checking md5 hash...".yellow);
@@ -53,11 +56,10 @@ exports.list_files = function (config, done, grunt) {
 			return;
 		}
 
-		var url = blob.path,
-			cmd = null,
-			args = ["ls-files", url];
+		var url = blob.path;
+		var cmd = null;
+		var args = ["ls-files", url];
 
-		grunt.log.writeln("Listing files under path:".yellow);
 
 		try {
 			cmd = spawn("git",args);
@@ -78,7 +80,7 @@ exports.list_files = function (config, done, grunt) {
 				if (!!value.match( blob.match || "" )) {
 					var foo = {
 						"path" : value,
-						"rev" : grunt.option("rev")||"HEAD"
+						"rev" : rev
 					};
 					return foo;
 				}
@@ -96,8 +98,12 @@ exports.list_files = function (config, done, grunt) {
 
 		cmd.on('close', function () {
 			file_list = unique(file_list);
-			var total = file_list.length ? ("("+file_list.length+") ").green : "(0) ".red
-			grunt.log.writeln(total + "- " + url);
+			grunt.log.writeln("- matching: '" + (blob.match||"") + "'");
+			if (file_list.length){
+				grunt.log.writeln(("("+file_list.length+") " + blob.path).green);
+			} else {
+				grunt.log.writeln(("(0) " + blob.path).red);
+			}
 			inspect_next_path();
 		});
 	}
@@ -114,36 +120,50 @@ exports.check_md5 = function (file_list, done, grunt) {
 		return;
 	}
 
-	var spawn          = require("child_process").spawn, md5sum;
+	var spawn = require("child_process").spawn;
+	var md5sum;
 	var queue =  [].concat(file_list), report = [];
+
+	function get_cmd_for(cmd, blob){
+		var bash;
+		var	args;
+		if (cmd === "git") {
+			args = ["show", blob.rev + ":" + blob.path];
+		} else{
+			args = ["show", blob.rev + ":" + blob.path];
+		}
+		try {
+			bash = spawn(cmd, args);
+		} catch (err) {
+			grunt.log.writeln(err);
+			return null;
+		}
+		return bash;
+	}
 
 	function cat_file (item) {
 
-		var cmd = null;
-		var	args = ["show", item.rev + ":" + item.path];
+		var cmd = get_cmd_for("git", item);
 		var hash = "";
 
-		try {
-			cmd = spawn("git", args);
-		} catch (err) {
-			grunt.log.writeln(err);
-			grunt.fail.fatal("Failed to run git command");
+		if (!cmd){
+			grunt.fail.fatal("Failed during command spawn.");
 			return;
 		}
 
 		cmd.stdout.on("data", function(data){
 			if (!data) {
-				grunt.fail.fatal("stdout:: failed to list files one '" + item.path + "'");
+				grunt.fail.fatal("stdout:: failed to list files on: '" + item.path + "'");
 				return;
 			}
 			hash += decoder.write(data);
 		});
 
 		cmd.stderr.on("data", function (data) {
-			grunt.log.writeln(data.toString().red);
+			grunt.log.writeln(("("+report.length+"/"+file_list.length+") " + item.path + " @stderr: " + trim(data.toString())).red );
 				var blob = {
 					file : item.path,
-					md5 : undefined
+					md5 : "--------------------------------"
 				};
 				report.push(blob);
 			return;
@@ -161,7 +181,7 @@ exports.check_md5 = function (file_list, done, grunt) {
 				};
 
 				report.push(blob);
-				grunt.log.writeln("+ " + item.path + " ("+report.length+"/"+file_list.length+") ");
+				grunt.log.writeln("("+report.length+"/"+file_list.length+") "+ item.path);
 			}
 			hash = null;
 			check_next_file();
