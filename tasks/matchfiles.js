d3701c01dc3ffc87cf899955c35fe562b6504713
igor <igor.p.almeida@gmail.com>
2015-03-18 15:43:59 -0300

    matchfiles working with a single local list

--- tasks/matchfiles.js
+++ tasks/matchfiles.js
@@ -1,16 +1,17 @@
 module.exports = function(grunt) {
 'use strict';
 
-	var mixIn          = require("mout/object/mixIn"),
+	var mixIn        = require("mout/object/mixIn"),
 		spawn          = require("child_process").spawn,
-		svn            = require("./svn"),
-		git            = require("./git"),
-		remote            = require("./remote"),
+		local          = require("./local"),
+		remote         = require("./remote"),
 		StringDecoder  = require('string_decoder').StringDecoder,
 		decoder        = new StringDecoder("utf8"),
 		forEach        = require("mout/array/forEach");
 
-	grunt.registerMultiTask('matchfiles', 'Match files against svn/git and hosted files. -branch for branch name and -rev for revision', function() {
+	grunt.registerMultiTask('matchfiles',
+		 	'Match files against svn/git and hosted files. -branch for branch name and -rev for revision',
+	function() {
 
 		var config = mixIn({
 			hosts : null,
@@ -18,8 +19,8 @@ module.exports = function(grunt) {
 			git : null
 		},this.data);
 
-		if (!config.hosts){
-			grunt.fail.fatal("No 'hosts' found.");
+		if (!config.hosts || !!config.hosts && !config.hosts.length){
+			grunt.fail.fatal("No 'hosts' found. this must be an array os strings");
 			return;
 		}
 
@@ -29,28 +30,22 @@ module.exports = function(grunt) {
 		}
 
 		var done = this.async();
+		var cmd = config.svn ? "svn" : "git";
 
 		function check_remote_files (value) {
 			grunt.log.writeln(("\nMatching md5 across hosts (" + config.hosts.length + ")").yellow);
 			remote.check({
-				cmd : config.svn ? "svn" : "git",
+				cmd : cmd,
 				files : value,
 				hosts : config.hosts
 			}, done, grunt);
 		}
 
-		//better object inspection
-		if (!!config.svn) {
-			svn.list_files(config.svn, function(file_list){
-				svn.check_md5(file_list, check_remote_files, grunt);
-			}	,grunt);
-		}
-	 	else if (!!config.git){
-			git.list_files(config.git, function(file_list){
-				git.check_md5(file_list, check_remote_files, grunt);
-			}, grunt);
-		}
+		local.list_files(cmd, config.git || config.svn, check_remote_files, grunt);
 
+		//local.list_files(cmd, config.git || config.svn, function(file_list){
+		//	local.check_md5(file_list, check_remote_files, grunt);
+		//}	,grunt);
 
 	});
 };
